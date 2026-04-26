2803d8eed4ef3a675104d5546f8924c8f04111a6
l3Zg0chAi <ngochaiuchihasenju@gmail.com>
2026-04-26 23:24:52 +0700

    .

--- rebuild.bat
+++ rebuild.bat
@@ -1,26 +1,6 @@
-@echo off
-cd /d "%~dp0"
-
-echo ===== CLEAN =====
-rmdir /s /q "build" 2>nul
-
-echo ===== CONFIGURE =====
-cmake -S . -B build
-
-if errorlevel 1 (
-    echo CMake configure loi.
-    pause
-    exit /b 1
-)
-
-echo ===== BUILD =====
-cmake --build build
-
-if errorlevel 1 (
-    echo Build loi.
-    pause
-    exit /b 1
-)
-
-echo ===== DONE =====
-pause
+#!/bin/bash
+CURRENT_DIR="$(pwd)"
+cd "$CURRENT_DIR"/build
+rm -rf *
+cmake -S .. -B .
+cmake --build .

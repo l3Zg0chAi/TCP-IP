50dd403c88b6635f71452714ceb5161f052a16b6
l3Zg0chAi <ngochaiuchihasenju@gmail.com>
2026-04-27 13:51:08 +0700

    script

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-echo "===== CLEAN ====="
 rm -rf "$SCRIPT_DIR/build"
 mkdir -p "$SCRIPT_DIR/build"
-echo "===== CONFIGURE ====="
 cmake -S "$SCRIPT_DIR" -B "$SCRIPT_DIR/build"
-echo "===== BUILD ====="
 cmake --build "$SCRIPT_DIR/build"
